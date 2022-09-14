07fd4bc9a957aceaad0da006e1e55769c7823ec7
AudunSorheim <audun.sorheim@nav.no>
2022-09-14 13:50:05 +0200

    wip

--- pages/sykmeldt/[oppfolgingsdialogId]/index.tsx
+++ pages/sykmeldt/[oppfolgingsdialogId]/index.tsx
@@ -1,3 +1,10 @@
 import OppfolgingsdialogSide from "../../../old-sykmeldt-frontend/components/oppfolgingsplan/OppfolgingsdialogSide";
+import {NextPage} from "next";
 
-export default <OppfolgingsdialogSide />
+const Page: NextPage = () => {
+    return (
+        <OppfolgingsdialogSide/>
+    );
+};
+
+export default Page
