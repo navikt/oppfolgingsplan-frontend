ed72592f416217a17c91d99cfaa656764b030b25
AudunSorheim <audun.sorheim@nav.no>
2022-09-23 15:12:14 +0200

    Litt opprydning

---
+++ src/common/components/tiltak/LagredeTiltak.tsx
@@ -0,0 +1,75 @@
+import {Tiltak} from "@/types/oppfolgingsplanservice/oppfolgingsplanTypes";
+import {TiltakPanel} from "@/common/components/tiltak/TiltakPanel";
+import {BodyLong, BodyShort, Button, Detail, Heading, Label, Tag} from "@navikt/ds-react";
+import {toDateMedMaanedNavn} from "@/common/utils/datoUtils";
+import {Delete, DialogDots, Edit} from "@navikt/ds-icons";
+import styled from "styled-components";
+import {ReactElement, ReactNode} from "react";
+
+interface Props {
+    tiltakListe: Tiltak[] | null
+}
+
+const HeadingWithLabel = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-direction: row;
+  margin-bottom: 1rem;
+`
+
+const ButtonRow = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const createStatusLabel = (statusText?: string | null): ReactElement | null => {
+    switch (statusText) {
+        case "FORSLAG":
+            return <Tag variant="warning" size="small">
+                Foreslått
+            </Tag>
+        case "AVBRUTT":
+            return <Tag variant="info" size="small">
+                Avbrutt
+            </Tag>
+        case "IKKE_AKTUELT":
+            return <Tag variant="error" size="small">
+                Ikke aktuelt
+            </Tag>
+        case "AVTALT":
+            return <Tag variant="success" size="small">
+                Avtalt
+            </Tag>
+    }
+    return null
+}
+
+export const LagredeTiltak = ({tiltakListe}: Props): ReactElement | null => {
+    if (!tiltakListe) return null
+
+    const alleTiltak = tiltakListe.map((tiltak, index) => {
+        return <TiltakPanel key={index} border={true}>
+            <HeadingWithLabel>
+                {createStatusLabel(tiltak.status)}
+                <Heading size={"medium"} level={"3"}>{tiltak.tiltaknavn}</Heading>
+            </HeadingWithLabel>
+
+            {tiltak.fom && tiltak.tom &&
+                <BodyShort
+                    spacing={true}>{toDateMedMaanedNavn(tiltak.fom)} - {toDateMedMaanedNavn(tiltak.tom)}</BodyShort>}
+
+            <Label>Beskrivelse</Label>
+            <BodyLong>{tiltak.beskrivelse}</BodyLong>
+
+            <Detail>{`Foreslått av ${tiltak.opprettetAv.navn}`}</Detail>
+
+            <ButtonRow>
+                <Button variant={"tertiary"} icon={<Edit/>}>Endre</Button>
+                <Button variant={"tertiary"} icon={<Delete/>}>Slett</Button>
+                <Button variant={"tertiary"} icon={<DialogDots/>}>Kommenter</Button>
+            </ButtonRow>
+        </TiltakPanel>
+    })
+
+    return <>{alleTiltak}</>
+}
--- src/sykmeldt/components/tiltak/tiltakContent.tsx
+++ src/sykmeldt/components/tiltak/tiltakContent.tsx
@@ -1,48 +1,9 @@
 import {Oppfolgingsplan} from "@/types/oppfolgingsplanservice/oppfolgingsplanTypes";
 import {NyttTiltak} from "@/common/components/tiltak/NyttTiltak";
-import {BodyLong, BodyShort, Button, Detail, Heading, Label, Tag} from "@navikt/ds-react";
-import {Delete, DialogDots, Edit} from "@navikt/ds-icons";
-import styled from "styled-components";
-import {TiltakPanel} from "@/common/components/tiltak/TiltakPanel";
-import {ReactElement} from "react";
-import {toDateMedMaanedNavn} from "@/common/utils/datoUtils";
+import {LagredeTiltak} from "@/common/components/tiltak/LagredeTiltak";
 
 interface Props {
     oppfolgingsplan: Oppfolgingsplan
-}
-
-const ButtonRow = styled.div`
-  display: flex;
-  flex-direction: row;
-`
-
-const HeadingWithLabel = styled.div`
-  display: flex;
-  gap: 1rem;
-  flex-direction: row;
-  margin-bottom: 1rem;
-`
-
-const createStatusLabel = (statusText?: string | null): ReactElement | null => {
-    switch (statusText) {
-        case "FORSLAG":
-            return <Tag variant="warning" size="small">
-                Foreslått
-            </Tag>
-        case "AVBRUTT":
-            return <Tag variant="info" size="small">
-                Avbrutt
-            </Tag>
-        case "IKKE_AKTUELT":
-            return <Tag variant="error" size="small">
-                Ikke aktuelt
-            </Tag>
-        case "AVTALT":
-            return <Tag variant="success" size="small">
-                Avtalt
-            </Tag>
-    }
-    return null
 }
 
 export const TiltakContent = ({oppfolgingsplan}: Props) => {
@@ -50,31 +11,7 @@ export const TiltakContent = ({oppfolgingsplan}: Props) => {
         <div>
             <NyttTiltak/>
 
-            {/*  Tiltakslista  */}
-            {oppfolgingsplan.tiltakListe.map((tiltak, index) => {
-                return <TiltakPanel key={index} border={true}>
-
-                    <HeadingWithLabel>
-                        {createStatusLabel(tiltak.status)}
-                        <Heading size={"medium"} level={"3"}>{tiltak.tiltaknavn}</Heading>
-                    </HeadingWithLabel>
-
-                    {tiltak.fom && tiltak.tom &&
-                        <BodyShort spacing={true}>{toDateMedMaanedNavn(tiltak.fom)} - {toDateMedMaanedNavn(tiltak.tom)}</BodyShort>}
-
-                    <Label>Beskrivelse</Label>
-                    <BodyLong>{tiltak.beskrivelse}</BodyLong>
-
-                    <Detail>{`Foreslått av ${tiltak.opprettetAv.navn}`}</Detail>
-
-                    <ButtonRow>
-                        <Button variant={"tertiary"} icon={<Edit/>}>Endre</Button>
-                        <Button variant={"tertiary"} icon={<Delete/>}>Slett</Button>
-                        <Button variant={"tertiary"} icon={<DialogDots/>}>Kommenter</Button>
-                    </ButtonRow>
-                </TiltakPanel>
-            })}
-
+            <LagredeTiltak tiltakListe={oppfolgingsplan.tiltakListe}/>
         </div>
     )
 }
