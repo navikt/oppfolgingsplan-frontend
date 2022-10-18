c584143df7b51f9788cea4bf3d6382a33004447b
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-10-18 10:23:40 +0200

    Correct sort order

--- src/common/components/arbeidsoppgaver/LagredeArbeidsoppgaver.tsx
+++ src/common/components/arbeidsoppgaver/LagredeArbeidsoppgaver.tsx
@@ -1,41 +1,32 @@
-import {ArbeidsoppgaveCard} from "@/common/components/arbeidsoppgaver/ArbeidsoppgaveCard";
-import {KANGJENNOMFOERES} from "@/common/konstanter";
+import { ArbeidsoppgaveCard } from "@/common/components/arbeidsoppgaver/ArbeidsoppgaveCard";
 import React from "react";
-import {Arbeidsoppgave} from "../../../schema/oppfolgingsplanSchema";
+import { Arbeidsoppgave } from "../../../schema/oppfolgingsplanSchema";
 
 interface Props {
-    arbeidstakerFnr: string;
-    arbeidsoppgaver: Arbeidsoppgave[];
+  arbeidstakerFnr: string;
+  arbeidsoppgaver: Arbeidsoppgave[];
 }
 
-export const LagredeArbeidsoppgaver = ({arbeidstakerFnr, arbeidsoppgaver}: Props) => {
+export const LagredeArbeidsoppgaver = ({
+  arbeidstakerFnr,
+  arbeidsoppgaver,
+}: Props) => {
+  arbeidsoppgaver.sort((a, b) => {
+    return b.arbeidsoppgaveId - a.arbeidsoppgaveId;
+  });
 
-    const sorterArbeidsoppgaverEtterTypeOgOpprettet = (arbeidsoppgaver: Arbeidsoppgave[]) => {
-        const order = [KANGJENNOMFOERES.KAN, KANGJENNOMFOERES.TILRETTELEGGING, KANGJENNOMFOERES.KAN_IKKE, KANGJENNOMFOERES.IKKE_VURDERT, undefined]
-        return arbeidsoppgaver.sort((a, b) => {
-            if (order.indexOf(b.gjennomfoering?.kanGjennomfoeres!) < order.indexOf(a.gjennomfoering?.kanGjennomfoeres!)) return 1;
-            if (order.indexOf(b.gjennomfoering?.kanGjennomfoeres!) > order.indexOf(a.gjennomfoering?.kanGjennomfoeres!)) return -1;
-            else {
-                return b.arbeidsoppgaveId - a.arbeidsoppgaveId;
-            }
-        });
-    };
-
-    sorterArbeidsoppgaverEtterTypeOgOpprettet(arbeidsoppgaver);
-
-    return (
-        <>
-            <div>
-                {arbeidsoppgaver.map((arbeidsoppgave: Arbeidsoppgave, idx: number) => (
-                    <ArbeidsoppgaveCard
-                        arbeidstakerFnr={arbeidstakerFnr}
-                        arbeidsoppgave={arbeidsoppgave}
-                        readonly={false}
-                        key={`arbeidsoppgaver-list-${idx}`}
-                    />
-                ))}
-            </div>
-        </>
-    );
-
+  return (
+    <>
+      <div>
+        {arbeidsoppgaver.map((arbeidsoppgave: Arbeidsoppgave, idx: number) => (
+          <ArbeidsoppgaveCard
+            arbeidstakerFnr={arbeidstakerFnr}
+            arbeidsoppgave={arbeidsoppgave}
+            readonly={false}
+            key={`arbeidsoppgaver-list-${idx}`}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
