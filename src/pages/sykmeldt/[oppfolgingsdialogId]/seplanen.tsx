26a5978076837df695ce56a805092ddb07a52ee9
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2022-10-20 15:31:26 +0200

    Add readonly toggle for SendTilGodkjenning (#21)
    Move SendTilGodkjenning one level up

--- src/pages/sykmeldt/[oppfolgingsdialogId]/seplanen.tsx
+++ src/pages/sykmeldt/[oppfolgingsdialogId]/seplanen.tsx
@@ -6,6 +6,7 @@ import {
 } from "@/common/components/wrappers/OppfolgingsplanPageSM";
 import { useAktivPlanSM } from "@/common/api/queries/sykmeldt/oppfolgingsplanerQueriesSM";
 import { OppfolgingsplanOversikt } from "@/common/components/oversikt/OppfolgingsplanOversikt";
+import { SendTilGodkjenning } from "../../../sykmeldt/components/oppfolgingsplan/sendTilGodkjenning/SendTilGodkjenning";
 
 const Seplanen: NextPage = () => {
   const aktivPlan = useAktivPlanSM();
@@ -13,6 +14,7 @@ const Seplanen: NextPage = () => {
   return (
     <OppfolgingsplanPageSM page={Page.SEPLANEN}>
       <OppfolgingsplanOversikt oppfolgingsplan={aktivPlan} />
+      <SendTilGodkjenning oppfolgingsplan={aktivPlan} />
     </OppfolgingsplanPageSM>
   );
 };
