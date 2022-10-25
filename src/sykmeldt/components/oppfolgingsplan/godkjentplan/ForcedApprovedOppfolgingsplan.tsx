bb41d23be4822e0af228852401ee85d442af83d3
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-10-25 10:01:17 +0200

    Add icon to ForcedApprovedOppfolgingsplan

--- src/sykmeldt/components/oppfolgingsplan/godkjentplan/ForcedApprovedOppfolgingsplan.tsx
+++ src/sykmeldt/components/oppfolgingsplan/godkjentplan/ForcedApprovedOppfolgingsplan.tsx
@@ -1,15 +1,12 @@
-import { BodyShort } from "@navikt/ds-react";
+import { ReportProblemCircleImage } from "@/common/images/imageComponents";
 import { NarmesteLeder } from "../../../../schema/narmestelederSchema";
+import { BildeTekstLinje } from "../godkjenn/BildeTekstLinje";
 
 interface Props {
   narmesteLeder: NarmesteLeder;
 }
 
 export const ForcedApprovedOppfolgingsplan = ({ narmesteLeder }: Props) => {
-  return (
-    <BodyShort spacing>
-      Planen er laget av arbeidsgiveren din. Er du uenig i innholdet, må du
-      snakke med {narmesteLeder.navn}.
-    </BodyShort>
-  );
+  const tekst = `Planen er laget av arbeidsgiveren din. Er du uenig i innholdet, må du snakke med ${narmesteLeder.navn}.`;
+  return <BildeTekstLinje imgUrl={ReportProblemCircleImage} tekst={tekst} />;
 };
