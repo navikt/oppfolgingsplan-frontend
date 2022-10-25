c4de4ac5e02cfb22065331a216975843fe7ca994
AudunSorheim <audun.sorheim@nav.no>
2022-10-25 10:53:52 +0200

    Fix PDF button

---
+++ src/sykmeldt/components/oppfolgingsplan/godkjentplan/AapnePlanSomPDF.tsx
@@ -0,0 +1,24 @@
+import { useApiBasePath } from "@/common/hooks/routeHooks";
+import { FileContent } from "@navikt/ds-icons";
+import { Button } from "@navikt/ds-react";
+
+interface Props {
+  oppfolgingsplanId: number;
+}
+
+export const AapnePlanSomPDF = ({ oppfolgingsplanId }: Props) => {
+  const apiBasePath = useApiBasePath();
+  const downloadPdfUrl = `${apiBasePath}/oppfolgingsplaner/${oppfolgingsplanId}/pdf`;
+
+  return (
+    <Button
+      onClick={() =>
+        window.open(downloadPdfUrl, "_blank", "noopener,noreferrer")
+      }
+      icon={<FileContent />}
+      variant="tertiary"
+    >
+      Åpne som pdf
+    </Button>
+  );
+};
