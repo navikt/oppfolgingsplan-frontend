2d7cae7835b8c1019376cd3bcb58347636758b05
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2022-11-01 13:17:20 +0100

    Buttons to share plan with nav and fastlege (#41)

---
+++ src/components/status/godkjentplan/DelMedNavOgFastlegeButtons.tsx
@@ -0,0 +1,67 @@
+import { Row } from "../../blocks/wrappers/Row";
+import { SpacedDiv } from "../../blocks/wrappers/SpacedDiv";
+import { Alert, Button } from "@navikt/ds-react";
+import {
+  useDelOppfolgingsplanMedFastlegeSM,
+  useDelOppfolgingsplanMedNavSM,
+} from "../../../api/queries/sykmeldt/oppfolgingsplanerQueriesSM";
+import { GodkjentPlan } from "../../../schema/oppfolgingsplanSchema";
+
+interface Props {
+  oppfolgingsplanId: number;
+  godkjentPlan: GodkjentPlan;
+}
+
+export const DelMedNavOgFastlegeButtons = ({
+  oppfolgingsplanId,
+  godkjentPlan,
+}: Props) => {
+  const delOppfolgingsplanMedFastlege = useDelOppfolgingsplanMedFastlegeSM();
+  const delOppfolgingsplanMedNAV = useDelOppfolgingsplanMedNavSM();
+
+  return (
+    <>
+      <Row marginBottom={"2rem"}>
+        {!godkjentPlan.deltMedNAV && (
+          <Button
+            variant={"secondary"}
+            loading={delOppfolgingsplanMedNAV.isLoading}
+            onClick={() => delOppfolgingsplanMedNAV.mutate(oppfolgingsplanId)}
+          >
+            Del med NAV
+          </Button>
+        )}
+        {!godkjentPlan.deltMedFastlege && (
+          <Button
+            variant={"secondary"}
+            loading={delOppfolgingsplanMedFastlege.isLoading}
+            onClick={() =>
+              delOppfolgingsplanMedFastlege.mutate(oppfolgingsplanId)
+            }
+          >
+            Del med fastlege
+          </Button>
+        )}
+      </Row>
+
+      {delOppfolgingsplanMedFastlege.isError && (
+        <SpacedDiv>
+          <Alert variant={"error"}>
+            Du får dessverre ikke delt planen med legen herfra. Det kan hende
+            fastlegen din ikke kan ta imot elektroniske meldinger. Eller har du
+            kanskje ingen fastlege? For å dele planen kan du laste den ned,
+            skrive den ut og ta den med deg neste gang du er hos legen.
+          </Alert>
+        </SpacedDiv>
+      )}
+
+      {delOppfolgingsplanMedNAV.isError && (
+        <SpacedDiv>
+          <Alert variant={"error"}>
+            Noe gikk feil da du prøvde å dele planen. Prøv igjen om litt.
+          </Alert>
+        </SpacedDiv>
+      )}
+    </>
+  );
+};
--- src/components/status/godkjentplanavbrutt/GodkjentPlanAvbrutt.tsx
+++ src/components/status/godkjentplanavbrutt/GodkjentPlanAvbrutt.tsx
@@ -3,14 +3,13 @@ import { Row } from "../../blocks/wrappers/Row";
 import { SpacedDiv } from "../../blocks/wrappers/SpacedDiv";
 import { AapnePlanSomPDF } from "../AapnePlanSomPDF";
 import { BothApprovedOppfolgingsplan } from "../godkjentplan/BothApprovedOppfolgingsplan";
-import { DelMedFastlegeKnapp } from "../godkjentplan/DelMedFastlegeKnapp";
-import { DelMedNavKnapp } from "../godkjentplan/DelMedNavKnapp";
 import { ForcedApprovedOppfolgingsplan } from "../godkjentplan/ForcedApprovedOppfolgingsplan";
 import { GodkjentPlanDeltBekreftelse } from "../godkjentplan/GodkjentPlanDeltBekreftelse";
 import { SePlan } from "../SePlan";
 import { TilLandingssideKnapp } from "../TilLandingssideKnapp";
 import { GodkjentPlanAvbruttTidspunkt } from "./GodkjentPlanAvbruttTidspunkt";
 import { TilGjeldendePlanKnapp } from "./TilGjeldendePlanKnapp";
+import { DelMedNavOgFastlegeButtons } from "../godkjentplan/DelMedNavOgFastlegeButtons";
 
 interface Props {
   oppfolgingsplan: Oppfolgingsplan;
@@ -38,22 +37,23 @@ export const GodkjentPlanAvbrutt = ({ oppfolgingsplan }: Props) => {
           narmesteLeder={oppfolgingsplan.arbeidsgiver.naermesteLeder}
         />
       )}
+
       <GodkjentPlanAvbruttTidspunkt godkjentPlan={godkjentPlan} />
+
       <GodkjentPlanDeltBekreftelse
         godkjentPlan={oppfolgingsplan.godkjentPlan}
       />
+
       <Row marginBottom={"2rem"}>
         <SePlan oppfolgingsplan={oppfolgingsplan} />
         <AapnePlanSomPDF oppfolgingsplanId={oppfolgingsplan.id} />
       </Row>
-      <Row marginBottom={"2rem"}>
-        {!oppfolgingsplan.godkjentPlan.deltMedNAV && (
-          <DelMedNavKnapp oppfolgingsplanId={oppfolgingsplan.id} />
-        )}
-        {!oppfolgingsplan.godkjentPlan.deltMedFastlege && (
-          <DelMedFastlegeKnapp oppfolgingsplanId={oppfolgingsplan.id} />
-        )}
-      </Row>
+
+      <DelMedNavOgFastlegeButtons
+        oppfolgingsplanId={oppfolgingsplan.id}
+        godkjentPlan={godkjentPlan}
+      />
+
       <TilLandingssideKnapp />
     </SpacedDiv>
   );
