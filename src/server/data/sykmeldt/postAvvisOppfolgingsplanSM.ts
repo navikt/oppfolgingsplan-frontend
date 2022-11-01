29e9cc0b64fbcd5d7f8b7463c336a8af403e731d
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-11-01 14:48:31 +0100

    Fix build

--- src/server/data/sykmeldt/postAvvisOppfolgingsplanSM.ts
+++ src/server/data/sykmeldt/postAvvisOppfolgingsplanSM.ts
@@ -19,7 +19,9 @@ export const postAvvisOppfolgingsplanSM = async (
   if (isMockBackend) {
     return next();
   } else {
-    const oppfolgingsplanTokenX = await getOppfolgingsplanTokenX(req);
+    const oppfolgingsplanTokenX = await getOppfolgingsplanTokenX(
+      req.idportenToken
+    );
 
     await avvisOppfolgingsplanSM(oppfolgingsplanTokenX, oppfolgingsplanId);
   }
