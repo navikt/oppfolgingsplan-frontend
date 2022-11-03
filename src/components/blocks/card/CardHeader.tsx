2f67209af754c73a4a365b481c674ed41970afd4
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2022-11-03 10:44:26 +0100

    Align cardheader icon and text (#50)

--- src/components/blocks/card/CardHeader.tsx
+++ src/components/blocks/card/CardHeader.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const CardHeader = styled.div`
   display: flex;
+  align-items: center;
   gap: 1rem;
   flex-direction: row;
   margin-bottom: 1rem;
