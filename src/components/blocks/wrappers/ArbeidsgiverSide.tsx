9c1ee3c44f656e633bf31aacbdb0a10c39c19b90
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2023-01-04 14:46:09 +0100

    Add sidemenu (#92)
    * Add sidemenu
    
    * Fix bad dekoratøren-styling
    
    * Move SM styling to SykmeldtSide

--- src/components/blocks/wrappers/ArbeidsgiverSide.tsx
+++ src/components/blocks/wrappers/ArbeidsgiverSide.tsx
@@ -1,12 +1,37 @@
 import React, { ReactElement, ReactNode } from "react";
 import { useDineSykmeldte } from "../../../api/queries/arbeidsgiver/dinesykmeldteQueriesAG";
-import { SpacedDiv } from "./SpacedDiv";
 import Feilmelding from "../error/Feilmelding";
 import AppSpinner from "../spinner/AppSpinner";
 import { PageHeading } from "../heading/PageHeading";
 import { useOppfolgingsplanerAG } from "../../../api/queries/arbeidsgiver/oppfolgingsplanerQueriesAG";
 import { useTilgangAG } from "../../../api/queries/arbeidsgiver/tilgangQueriesAG";
 import { AdresseSperreInfoBoks } from "../infoboks/AdresseSperreInfoBoks";
+import { ArbeidsgiverSideMenu } from "../sidemenu/ArbeidsgiverSideMenu";
+import { PageContainer } from "@navikt/dinesykmeldte-sidemeny";
+import { Sykmeldt } from "../../../schema/sykmeldtSchema";
+import { addSpaceAfterEverySixthCharacter } from "../../../utils/stringUtils";
+import { People } from "@navikt/ds-icons";
+
+const getSykmeldtHeader = (sykmeldt?: Sykmeldt) => {
+  if (sykmeldt?.navn && sykmeldt.fnr) {
+    return {
+      title: sykmeldt.navn,
+      subtitle: `Fødselsnr: ${addSpaceAfterEverySixthCharacter(sykmeldt?.fnr)}`,
+      Icon: People,
+    };
+  }
+
+  return false;
+};
+
+const getSykmeldtNameAndFnr = (sykmeldt?: Sykmeldt) => {
+  if (!sykmeldt) return null;
+
+  return {
+    navn: sykmeldt.navn || "",
+    fnr: sykmeldt.fnr,
+  };
+};
 
 interface SideProps {
   title: string;
@@ -26,12 +51,10 @@ const ArbeidsgiverSide = ({
   const PageContent = () => {
     if (oppfolgingsplaner.isError || sykmeldt.isError || tilgang.isError) {
       return (
-        <SpacedDiv>
-          <Feilmelding
-            title="Beklager, vi fikk en teknisk feil"
-            description="Det skjedde en feil ved henting av dine oppfølgingsplaner. Vennligst prøv igjen senere."
-          />
-        </SpacedDiv>
+        <Feilmelding
+          title="Beklager, vi fikk en teknisk feil"
+          description="Det skjedde en feil ved henting av dine oppfølgingsplaner. Vennligst prøv igjen senere."
+        />
       );
     } else if (
       !sykmeldt.isError &&
@@ -52,7 +75,15 @@ const ArbeidsgiverSide = ({
     }
   };
 
-  return <PageContent />;
+  return (
+    <PageContainer
+      sykmeldt={getSykmeldtNameAndFnr(sykmeldt.data)}
+      header={getSykmeldtHeader(sykmeldt.data)}
+      navigation={<ArbeidsgiverSideMenu sykmeldt={sykmeldt.data} />}
+    >
+      <PageContent />
+    </PageContainer>
+  );
 };
 
 export default ArbeidsgiverSide;
