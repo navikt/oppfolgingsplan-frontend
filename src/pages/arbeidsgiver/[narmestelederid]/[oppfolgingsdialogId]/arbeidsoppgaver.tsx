db9035244e4e40e2825b17933d7fd38a6a0877cc
Vitenok <victorandreevi4@gmail.com>
2023-01-11 11:44:52 +0100

    QA: add error mesaage box if OP missing employee's fnr

--- src/pages/arbeidsgiver/[narmestelederid]/[oppfolgingsdialogId]/arbeidsoppgaver.tsx
+++ src/pages/arbeidsgiver/[narmestelederid]/[oppfolgingsdialogId]/arbeidsoppgaver.tsx
@@ -9,11 +9,23 @@ import {
 import { LagredeArbeidsoppgaver } from "components/arbeidsoppgaver/LagredeArbeidsoppgaver";
 import { beskyttetSideUtenProps } from "../../../../auth/beskyttetSide";
 import { NyArbeidsoppgaveAG } from "../../../../components/arbeidsoppgaver/NyArbeidsoppgaveAG";
+import {SpacedDiv} from "../../../../components/blocks/wrappers/SpacedDiv";
+import Feilmelding from "../../../../components/blocks/error/Feilmelding";
 
 const Arbeidsoppgaver: NextPage = () => {
   const aktivPlan = useChosenAktivOppfolgingsplanAG();
   const arbeidstakerFnr = aktivPlan?.arbeidstaker.fnr;
-  if (!arbeidstakerFnr) return null;
+  if (!arbeidstakerFnr) {
+    return (
+        <SpacedDiv>
+          <Feilmelding
+              description={
+                "Oppfolgingsplan mangler arbeidstakers fødselsnummer. Vennligst prøv igjen senere."
+              }
+          />
+        </SpacedDiv>
+    )
+  }
 
   return (
     <OppfolgingsplanPageAG page={Page.ARBEIDSOPPGAVER}>
--- src/pages/sykmeldt/[oppfolgingsdialogId]/arbeidsoppgaver.tsx
+++ src/pages/sykmeldt/[oppfolgingsdialogId]/arbeidsoppgaver.tsx
@@ -9,12 +9,24 @@ import {
 import { LagredeArbeidsoppgaver } from "components/arbeidsoppgaver/LagredeArbeidsoppgaver";
 import { NyArbeidsoppgaveSM } from "components/arbeidsoppgaver/NyArbeidsoppgaveSM";
 import { beskyttetSideUtenProps } from "../../../auth/beskyttetSide";
+import {SpacedDiv} from "../../../components/blocks/wrappers/SpacedDiv";
+import Feilmelding from "../../../components/blocks/error/Feilmelding";
 
 const Arbeidsoppgaver: NextPage = () => {
   const aktivPlan = useAktivPlanSM();
 
   const arbeidstakerFnr = aktivPlan?.arbeidstaker.fnr;
-  if (!arbeidstakerFnr) return null;
+  if (!arbeidstakerFnr) {
+    return  (
+        <SpacedDiv>
+          <Feilmelding
+              description={
+                "Oppfolgingsplan mangler arbeidstakers fødselsnummer. Vennligst prøv igjen senere."
+              }
+          />
+        </SpacedDiv>
+    )
+  }
 
   return (
     <OppfolgingsplanPageSM page={Page.ARBEIDSOPPGAVER}>
