57c50ad98e5d7a2c26acbb9e8e2bffb6da28b9d9
Luan Tran <8613937+luantr@users.noreply.github.com>
2023-01-12 10:02:37 +0100

    Add nullable to tekst field in schema (#113)

--- src/schema/oppfolgingsplanSchema.ts
+++ src/schema/oppfolgingsplanSchema.ts
@@ -43,7 +43,7 @@ export const personSchema = object({
 
 export const kommentarSchema = object({
   id: number(),
-  tekst: string(),
+  tekst: string().nullable(),
   opprettetTidspunkt: string(),
   sistEndretDato: string(),
   opprettetAv: personSchema,
