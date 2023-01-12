2e9c5cc09b9560cad851384ad2e5b437960c2769
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2023-01-12 07:58:49 +0100

    Sletter ubrukt mappe (#110)

--- src/server/data/mock/testscenarios/tidligerePlaner/tidligerePlanerScenario.ts
+++
@@ -1,9 +0,0 @@
-import { MockSetup } from "../../getMockDb";
-import { defaultMockSetup } from "../../defaultData/defaultMockSetup";
-import {tidligerePlanerMockData} from "./tidligerePlanMockData";
-
-export const tidligerePlanerScenario: MockSetup = {
-  ...defaultMockSetup,
-  oppfolgingsplaner: tidligerePlanerMockData,
-  activeTestScenario: "TIDLIGEREPLANER",
-};
