817c5f7fccaaa3d911b3aa6f65f0b283cc022518
Luan Tran <8613937+luantr@users.noreply.github.com>
2023-01-20 11:07:32 +0100

    Fix linting errors

--- src/server/data/mock/testscenarios/tidligereoppfolgingsplaner/tidligerePlanerScenario.ts
+++ src/server/data/mock/testscenarios/tidligereoppfolgingsplaner/tidligerePlanerScenario.ts
@@ -1,6 +1,6 @@
 import { MockSetup } from "../../getMockDb";
 import { defaultMockSetup } from "../../defaultData/defaultMockSetup";
-import {tidligerePlanerMockData} from "./tidligerePlanMockData";
+import { tidligerePlanerMockData } from "./tidligerePlanMockData";
 
 export const tidligerePlanerScenario: MockSetup = {
   ...defaultMockSetup,
