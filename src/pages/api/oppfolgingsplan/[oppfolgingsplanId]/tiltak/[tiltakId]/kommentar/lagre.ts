32d914fc8c9be12a020ad23caecb115f5209010a
Tanja Krasnomovets <victorandreevi4@gmail.com>
2023-01-24 13:34:05 +0100

    Fix imports. TODO: deleteTiltakCommentSM

--- src/pages/api/oppfolgingsplan/[oppfolgingsplanId]/tiltak/[tiltakId]/kommentar/lagre.ts
+++ src/pages/api/oppfolgingsplan/[oppfolgingsplanId]/tiltak/[tiltakId]/kommentar/lagre.ts
@@ -1,10 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { isMockBackend } from "../../../../../../../../environments/publicEnv";
-import { getSyfoOppfolgingsplanserviceTokenFromRequest } from "../../../../../../../../server/auth/tokenx/getTokenXFromRequest";
-import { getTiltakIdFromRequest } from "../../../../../../../../server/utils/requestUtils";
-import { saveTiltakCommentSM } from "../../../../../../../../server/service/oppfolgingsplanService";
-import { beskyttetApi } from "../../../../../../../../server/auth/beskyttetApi";
-import { Kommentar } from "../../../../../../../../types/oppfolgingsplan";
+import {isMockBackend} from "../../../../../../../environments/publicEnv";
+import {
+  getSyfoOppfolgingsplanserviceTokenFromRequest
+} from "../../../../../../../server/auth/tokenx/getTokenXFromRequest";
+import {getTiltakIdFromRequest} from "../../../../../../../server/utils/requestUtils";
+import {Kommentar} from "../../../../../../../types/oppfolgingsplan";
+import {saveTiltakCommentSM} from "../../../../../../../server/service/oppfolgingsplanService";
+import {beskyttetApi} from "../../../../../../../server/auth/beskyttetApi";
 
 const handler = async (
   req: NextApiRequest,
