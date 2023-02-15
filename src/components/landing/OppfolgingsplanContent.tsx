e9803f76a4fc287abc2073e65a9357c0d9382ab8
AudunSorheim <audun.sorheim@nav.no>
2023-02-15 14:27:13 +0100

    Display infobox when no sykmelding

--- src/components/landing/OppfolgingsplanContent.tsx
+++ src/components/landing/OppfolgingsplanContent.tsx
@@ -1,17 +1,7 @@
 import React from "react";
-import {
-  erSykmeldtUtenOppfolgingsplanerOgNaermesteLedere,
-  finnTidligereOppfolgingsplaner,
-  harTidligereOppfolgingsplaner,
-} from "utils/oppfolgingplanUtils";
+import { erSykmeldtUtenOppfolgingsplanerOgNaermesteLedere } from "utils/oppfolgingplanUtils";
 import { Sykmelding } from "../../schema/sykmeldingSchema";
-import {
-  sykmeldtHarGyldigSykmelding,
-  sykmeldtHarIngenSendteSykmeldinger,
-} from "utils/sykmeldingUtils";
 import OppfolgingsdialogerVisning from "./teaser/OppfolgingsdialogerVisning";
-import OppfolgingsdialogerUtenAktivSykmelding from "./OppfolgingsdialogerUtenAktivSykmelding";
-import OppfolgingsplanUtenGyldigSykmelding from "./OppfolgingsplanUtenGyldigSykmelding";
 import { IngenLedereInfoBoks } from "components/blocks/infoboks/IngenLedereInfoBoks";
 import { NarmesteLeder, Oppfolgingsplan } from "../../types/oppfolgingsplan";
 
@@ -26,26 +16,7 @@ const OppfolgingsplanContent = ({
   sykmeldinger,
   narmesteLedere,
 }: Props) => {
-  if (!sykmeldtHarGyldigSykmelding(sykmeldinger)) {
-    return (
-      <div>
-        <OppfolgingsplanUtenGyldigSykmelding
-          sykmeldtHarIngenSendteSykmeldinger={sykmeldtHarIngenSendteSykmeldinger(
-            sykmeldinger
-          )}
-        />
-
-        {oppfolgingsplaner &&
-          harTidligereOppfolgingsplaner(oppfolgingsplaner) && (
-            <OppfolgingsdialogerUtenAktivSykmelding
-              oppfolgingsplanerUtenAktivSykmelding={finnTidligereOppfolgingsplaner(
-                oppfolgingsplaner
-              )}
-            />
-          )}
-      </div>
-    );
-  } else if (
+  if (
     erSykmeldtUtenOppfolgingsplanerOgNaermesteLedere(
       oppfolgingsplaner,
       sykmeldinger,
