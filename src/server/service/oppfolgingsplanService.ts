f799717d6a817ad90a199608a635d646738c2854
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2023-02-16 10:13:20 +0100

    Only fetch planer for given fnr (#169)
    Only fetch sykmeldtes planer

--- src/server/service/oppfolgingsplanService.ts
+++ src/server/service/oppfolgingsplanService.ts
@@ -112,10 +112,13 @@ export async function getOppfolgingsplanerSM(accessToken: string) {
   handleSchemaParsingError("Sykmeldt", "Oppfolgingsplan", response.error);
 }
 
-export async function getOppfolgingsplanerAG(accessToken: string) {
+export async function getOppfolgingsplanerAG(
+  sykmeldtFnr: string,
+  accessToken: string
+) {
   const response = array(oppfolgingsplanSchema).safeParse(
     await get(
-      `${serverEnv.SYFOOPPFOLGINGSPLANSERVICE_HOST}/syfooppfolgingsplanservice/api/v2/arbeidsgiver/oppfolgingsplaner`,
+      `${serverEnv.SYFOOPPFOLGINGSPLANSERVICE_HOST}/syfooppfolgingsplanservice/api/v2/arbeidsgiver/oppfolgingsplaner/${sykmeldtFnr}`,
       {
         accessToken,
       }
