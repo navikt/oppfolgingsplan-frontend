61452c3df0cc09a7c87f8195cdc3325309a0987f
AudunSorheim <audun.sorheim@nav.no>
2023-03-20 08:49:54 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const moduleExports = {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
+  pageExtensions: ["!.cy.ts(x)"],
 };
 
 module.exports = moduleExports;
