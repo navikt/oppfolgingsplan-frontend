dea9bd25cd30e7193f2e8eba110223fb56121b82
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2023-06-07 10:38:17 +0200

    Fix environment variables (#219)
    * Fix environment variables

--- src/environments/publicEnv.ts
+++ src/environments/publicEnv.ts
@@ -1,24 +1,5 @@
 export const basePath = process.env.NEXT_PUBLIC_BASEPATH;
 
-const devOrigin = "https://www-gcp.dev.nav.no";
-const demoOrigin = "https://demo.ekstern.dev.nav.no";
-
-export const isDemo = () => {
-  if (typeof window !== "undefined") {
-    return window.location.origin === demoOrigin;
-  }
-
-  return process.env.RUNTIME_ENVIRONMENT === "demo";
-};
-
-export const isDev = () => {
-  if (typeof window !== "undefined") {
-    return window.location.origin === devOrigin;
-  }
-
-  return process.env.RUNTIME_ENVIRONMENT === "dev";
-};
-
 export const dineSykemeldteRoot = (): string => {
   return process.env.NEXT_PUBLIC_DINE_SYKMELDTE_URL || "";
 };
@@ -27,7 +8,8 @@ export const dittSykefravarRoot = (): string => {
   return process.env.NEXT_PUBLIC_DITT_SYKEFRAVAER_URL || "";
 };
 export const displayTestScenarioSelector =
-  process.env.NEXT_PUBLIC_IS_DEVELOPMENT === "true" || isDemo();
+  process.env.NEXT_PUBLIC_IS_DEVELOPMENT === "true" ||
+  process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT === "demo";
 
 export const cdnPublicPath: string | undefined = process.env
   .NEXT_PUBLIC_ASSET_PREFIX
