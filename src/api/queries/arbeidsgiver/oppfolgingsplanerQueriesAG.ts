c1167efd4d96cb32e4e94fd8c86877fd06244b6a
AudunSorheim <audun.sorheim@nav.no>
2023-06-27 12:08:09 +0200

    Dependent query OP AG

--- src/api/queries/arbeidsgiver/oppfolgingsplanerQueriesAG.ts
+++ src/api/queries/arbeidsgiver/oppfolgingsplanerQueriesAG.ts
@@ -34,7 +34,7 @@ export const useOppfolgingsplanerAG = () => {
     [queryKeys.OPPFOLGINGSPLANER],
     fetchOppfolgingsplaner,
     {
-      enabled: !!sykmeldtFnr,
+      enabled: !!sykmeldtFnr && !!narmesteLederId,
       useErrorBoundary: true,
     }
   );
