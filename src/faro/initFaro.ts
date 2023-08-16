bc13cf0e903d6a86fa8a42afbb62b7942e7226f7
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2023-08-16 08:37:53 +0200

    Refactor faro log placement (#249)

--- src/faro/initFaro.ts
+++ src/faro/initFaro.ts
@@ -4,9 +4,6 @@ import {
   initializeFaro,
 } from "@grafana/faro-react";
 
-// eslint-disable-next-line
-declare const window: any;
-
 export const initFaro = (): Faro | null => {
   if (!process.env.NEXT_PUBLIC_TELEMETRY_URL || typeof window === "undefined")
     return null;
@@ -23,16 +20,4 @@ export const initFaro = (): Faro | null => {
       }),
     ],
   });
-};
-
-export const logFaroError = (err: Error) => {
-  if (typeof window !== "undefined" && !!window.faro) {
-    window.faro.api.pushError(err);
-  }
-};
-
-export const logFaroErrorMessage = (message: string) => {
-  if (typeof window !== "undefined" && !!window.faro) {
-    window.faro.api.pushError(new Error(message));
-  }
 };
--- src/utils/logUtils.ts
+++ src/utils/logUtils.ts
@@ -1,10 +1,24 @@
-import { logFaroErrorMessage } from "../faro/initFaro";
 import { logger } from "@navikt/next-logger";
+
+// eslint-disable-next-line
+declare const window: any;
 
 export const logError = (message: string) => {
   if (typeof window !== "undefined") {
     logFaroErrorMessage(message);
   } else {
     logger.error(message);
+  }
+};
+
+export const logFaroError = (err: Error) => {
+  if (typeof window !== "undefined" && !!window.faro) {
+    window.faro.api.pushError(err);
+  }
+};
+
+export const logFaroErrorMessage = (message: string) => {
+  if (typeof window !== "undefined" && !!window.faro) {
+    window.faro.api.pushError(new Error(message));
   }
 };
