79a9f7bd933c2e1b6ea583fc6a41696c64e5ae52
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2024-03-14 14:22:04 +0100

    Log data if 409-error (#605)

--- src/components/status/GodkjennOppfolgingsplan.tsx
+++ src/components/status/GodkjennOppfolgingsplan.tsx
@@ -2,24 +2,22 @@ import { Alert, BodyLong, Button, Checkbox, Heading } from "@navikt/ds-react";
 import { useState } from "react";
 import { SpacedDiv } from "../blocks/wrappers/SpacedDiv";
 import { useGodkjennsistOppfolgingsplan } from "../../api/queries/oppfolgingsplan/oppfolgingsplanQueries";
-import { GodkjenningDTO } from "../../schema/oppfolgingsplanSchema";
+import { OppfolgingsplanDTO } from "../../schema/oppfolgingsplanSchema";
 
 export type MotpartNavnForAltinn = "arbeidstakeren" | "arbeidsgiveren din";
 
 interface Props {
-  oppfolgingsplanId: number;
+  oppfolgingsplan: OppfolgingsplanDTO;
   motpartNavnForAltinn: MotpartNavnForAltinn;
-  godkjenninger: GodkjenningDTO[];
 }
 
 export const GodkjennOppfolgingsplan = ({
-  oppfolgingsplanId,
+  oppfolgingsplan,
   motpartNavnForAltinn,
-  godkjenninger,
 }: Props) => {
   const [delMedNav, setDelMedNav] = useState(false);
   const godkjennOppfolgingsplan =
-    useGodkjennsistOppfolgingsplan(oppfolgingsplanId);
+    useGodkjennsistOppfolgingsplan(oppfolgingsplan);
 
   return (
     <SpacedDiv>
@@ -31,7 +29,9 @@ export const GodkjennOppfolgingsplan = ({
         automatisk bli tilgjengelige for arbeidsplassen i Altinn.
       </BodyLong>
       <SpacedDiv>
-        {godkjenninger.find((godkjenning) => godkjenning.delMedNav) ? (
+        {oppfolgingsplan.godkjenninger.find(
+          (godkjenning) => godkjenning.delMedNav,
+        ) ? (
           <Alert variant="info">
             Planen vil bli delt med NAV når du godkjenner den.
           </Alert>
