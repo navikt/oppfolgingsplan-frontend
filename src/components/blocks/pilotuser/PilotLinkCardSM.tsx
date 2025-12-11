32b51aa8ac5da25081420b574dbeb7de0a8034d6
Daniel Engebretsen <daniel.engebretsen@nav.no>
2025-12-11 14:13:23 +0100

    enable pilot in prod for SM

--- src/components/blocks/pilotuser/PilotLinkCardSM.tsx
+++ src/components/blocks/pilotuser/PilotLinkCardSM.tsx
@@ -1,14 +1,10 @@
 import { useIsPilotSM } from "../../../api/queries/sykmeldt/pilotQueriesSM";
-import { isProd, nyOppfolgingsplanRoot } from "../../../environments/publicEnv";
+import { nyOppfolgingsplanRoot } from "../../../environments/publicEnv";
 import { LinkCard } from "@navikt/ds-react";
 import { ClipboardLinkIcon } from "@navikt/aksel-icons";
 
 export const PilotLinkCardSM = () => {
   const isPilotUserQuery = useIsPilotSM();
-
-  if (isProd) {
-    return null;
-  }
 
   if (isPilotUserQuery.isSuccess && isPilotUserQuery.data == true) {
     return (
